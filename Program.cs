ca87818c5e1c25603e94474243b8be2818aaf92b
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-03-11 15:50:37 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace G_NET_27_OOP_6
+﻿using System;
+
+namespace G_NET_27_OOP_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            // Abstraction is the process of exposing only what the user needs and hiding how it is implemented.
+            // Focus on what the object does not how it does it .
+
+            //Abstraction: Focus What an object does & Hide Implementation , Encapsulation : Focus on How Can Protected data.
+            //
+            // Abstraction
+            // Coffee Machine 
+            // — What you can do (don't care how it works internally)
+            // Press the coffee button
+            // Choose the size (small, medium, large)
+            // Add sugar
+            // Collect your coffee
+
+            // What you don't care about
+            // How the water heating system works
+            // How the beans are ground
+            // How the pressure pump operates
+
+            //Encapsulation 
+            // Water level and beans are private
+
+            #endregion
+
         }
     }
 }
